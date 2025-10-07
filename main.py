508d51687adfa8479e8936ad0630aa8a409774cc
masovs9-creator <masovs9@gmail.com>
2025-10-07 21:47:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Бот запущен и сервер работает!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print(data)
+    return "ok", 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
